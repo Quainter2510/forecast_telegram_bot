18af5981e6c02e3fb598fca962d047c46d696423
Paul <nihgpavel@yandex.ru>
2024-06-10 20:29:11 +0300

    test4 schedule

--- loader.py
+++ loader.py
@@ -1,10 +1,13 @@
 from telebot import TeleBot, apihelper
 from config_data import config
 from database.common import MyDataBase
+import handlers
 
 
 # apihelper.SESSION_TIME_TO_LIVE = 5 * 60
 bot = TeleBot(token=config.BOT_TOKEN)
 base = MyDataBase()
+
+handlers.custom_handlers.reminder.reminder()
 
 
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         try:
             bot.polling(none_stop=True)
             # handlers.custom_handlers.reminder.reminder()
-            schedule.every().minute.do(handlers.custom_handlers.reminder.reminder)
+            # schedule.every().minute.do(handlers.custom_handlers.reminder.reminder)
         except Exception as exc:
             bot.send_message(config.ADMIN_ID, exc, reply_markup=my_marcup.main_menu_marcup())
 
