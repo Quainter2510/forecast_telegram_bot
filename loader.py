8d8af393716128e15a2aec574c21f5eed5ca373a
Paul <nihgpavel@yandex.ru>
2024-06-10 20:23:36 +0300

    test 2 schadule

--- loader.py
+++ loader.py
@@ -1,15 +1,6 @@
 from telebot import TeleBot, apihelper
 from config_data import config
 from database.common import MyDataBase
-import schedule
-from handlers.custom_handlers import reminder
-
-# Define a function to print a message
-
-
-
-# Schedule the task to run every day at 7:00 AM
-schedule.every().minute.do(reminder)
 
 
 # apihelper.SESSION_TIME_TO_LIVE = 5 * 60
