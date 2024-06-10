833df0728aae3f3007e5c401140950360417dd22
Paul <nihgpavel@yandex.ru>
2024-06-10 20:26:23 +0300

    test3 schedule

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     while True:
         try:
             bot.polling(none_stop=True)
-            schedule.every().minute.do(handlers.custom_handlers.reminder)
+            # handlers.custom_handlers.reminder.reminder()
+            schedule.every().minute.do(handlers.custom_handlers.reminder.reminder)
         except Exception as exc:
             bot.send_message(config.ADMIN_ID, exc, reply_markup=my_marcup.main_menu_marcup())
 
