cb31e5ae1c0703520ae701af69202349e11262a3
Paul <nihgpavel@yandex.ru>
2024-06-15 08:03:01 +0300

    add reminder and updater

--- reminder.py
+++ reminder.py
@@ -1,9 +1,19 @@
 from config_data.config import ADMIN_ID
 from loader import bot, base
+import datetime
+import time
 
-bot.send_message(ADMIN_ID, "reminder start")
-players = base.reminder(base.get_now_tour())
-for elem in players:
-    bot.send_message(elem[0], "Матчи скоро начнутся. Не забудьте сделать прогноз")
-    bot.send_message(ADMIN_ID, base.get_nickname_player(elem[0]) + " не сделал прогноз")
-bot.send_message(ADMIN_ID, "reminder finish")
+def reminder():
+    bot.send_message(ADMIN_ID, "reminder start")
+    players = base.reminder(base.get_now_tour())
+    for elem in players:
+        bot.send_message(elem[0], "Матчи скоро начнутся. Не забудьте сделать прогноз")
+        bot.send_message(ADMIN_ID, base.get_nickname_player(elem[0]) + " не сделал прогноз")
+    bot.send_message(ADMIN_ID, "reminder finish")
+
+while True:
+    if datetime.datetime.now().day in (15, 16, 17, 19, 20, 21, 22) and datetime.datetime.now().hour == 15 or \
+        datetime.datetime.now().day in (18, 25, 26, 29, 30, 1, 2, 5, 6) and datetime.datetime.now().hour == 18 or \
+        datetime.datetime.now().day in (23, 24, 14, 10, 9) and datetime.datetime.now().hour == 21:
+        reminder()
+        time.sleep(10)
--- updater.py
+++ updater.py
@@ -1,8 +1,15 @@
 from loader import base
+import datetime
+import time
 
+def update():
+    base.update_result_tour()
+    currtour = base.get_now_tour()
+    for id in base.get_all_id_player():
+        base.update_tournament_table(id[0], currtour - 1, base.number_of_points_per_tour(id[0], currtour - 1))
+        base.update_tournament_table(id[0], currtour, base.number_of_points_per_tour(id[0], currtour))
 
-base.update_result_tour()
-currtour = base.get_now_tour()
-for id in base.get_all_id_player():
-    base.update_tournament_table(id[0], currtour - 1, base.number_of_points_per_tour(id[0], currtour - 1))
-    base.update_tournament_table(id[0], currtour, base.number_of_points_per_tour(id[0], currtour))
+while True:
+    if datetime.datetime.now().min % 3 == 0:
+        update()
+        time.sleep(70)
