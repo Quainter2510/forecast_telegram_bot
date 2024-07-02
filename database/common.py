5549fff0d6be417a910268ea42fd9e0ab4eaa604
Paul <nihgpavel@yandex.ru>
2024-07-02 22:36:55 +0300

    fix last tour

--- database/common.py
+++ database/common.py
@@ -183,7 +183,7 @@ class MyDataBase:
         # Вернуть текущий тур
         tour = self.cursor.execute(f"SELECT tour FROM matches WHERE date >= datetime('now','localtime')").fetchone()
         if tour == None:
-            return config.NUMBER_OF_TOUR
+            return self.cursor.execute(f"SELECT MAX(tour) FROM matches").fetchone()
         return tour[0]
 
     def get_all_id_player(self) -> Tuple:
